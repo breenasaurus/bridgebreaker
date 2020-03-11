871654a3626148de36773ed2be91b6f54e83bc44
Breena Hughes <breena.hughes@yahoo.com>
2020-03-11 13:19:09 -0500

    added the Start info box and the new window

--- testgui.py
+++ testgui.py
@@ -1,19 +1,25 @@
 
-from guizero import App, Text, TextBox, PushButton
+from guizero import App, Text, TextBox, PushButton, info, Window
 
 # Creates the GUI
 app = App(title="Start Screen")
 
 # Creates the text instruction
-instruction = Text(app, text="Please type in Team ID number and then press Start", size=28, font="Times New Roman")
+instruction = Text(app, text="Please type in Team ID number,\n and then press Start", size=28, font="Times New Roman")
 
 # Creates TShe text box to enter the ID number
 IDbox = TextBox(app, width=5)
 
+# Sets team ID to 0 before ID number placed
+teamID = 00000
+
 # Creates the start button
 def start_button():
-  IDbox.value = teamID.value
-  update_ID = PushButton(app, command = start_button, text="Start bridge test!")
+  IDbox = int(teamID)
+  info("Testing", "Testing the Bridge")
+  actual_test = Window(app, title = "Emergency Stop")
+  
+update_ID = PushButton(app, command = start_button, text="Start bridge test!")
 
 # Need to find a library that creates the touchscreen 
 # Make the start button bigger
