93e86d0ad1bfd0883c40cd8f2c83e93939a8d05d
breenasaurus <breena.hughes@yahoo.com>
2020-03-11 16:58:21 +0000

    added start button

--- testgui.py
+++ testgui.py
@@ -5,10 +5,15 @@ from guizero import App, Text, TextBox, PushButton
 app = App(title="Start Screen")
 
 # Creates the text instruction
-instruction = Text(app, text="Please type in Team ID number and then press Start", size=40, font="Times New Roman")
+instruction = Text(app, text="Please type in Team ID number and then press Start", size=28, font="Times New Roman")
 
 # Creates TShe text box to enter the ID number
 IDbox = TextBox(app, width=5)
+
+# Creates the start button
+def start_button():
+  IDbox.value = teamID.value
+  update_ID = PushButton(app, command = start_button, text="Start bridge test!")
 
 # Need to find a library that creates the touchscreen 
 # Make the start button bigger
