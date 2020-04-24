bdd24ee2fd2cfe42be4d3f0c3b3b4a785889f255
breenasaurus <62038408+breenasaurus@users.noreply.github.com>
2020-04-24 04:29:59 -0500

    Update GUI
    includes a weight display, hopefully.

--- testgui.py
+++ testgui.py
@@ -13,17 +13,21 @@ IDbox = TextBox(app, width=5)
 # Sets team ID to 0 before ID number placed
 teamID = 00000
 
+# Sets initial weight to 0, time to 1 second and command to fetch weight
+weight = 0
+time = 1000
+def fetchweight():
+
 # Creates the start button
 def start_button():
   IDbox = int(teamID)
   info("Testing", "Testing the Bridge")
   actual_test = Window(app, title = "Emergency Stop")
   text = Text(actual_test, text="In case of emergency please press the emergency stop.")
+  weightdisplay = (actual_test, text = weight,repeat(time, fetchweight, args=None)
   emergency = PushButton(actual_test,command = actual_test.hide, text="EMERGENCY STOP!")
   
 update_ID = PushButton(app, command = start_button, text="Start bridge test!")
 
-# Need to find a library that creates the touchscreen 
-# Make the start button bigger
-def begintest():
+
     app.display()
