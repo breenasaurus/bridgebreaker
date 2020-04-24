0543ea92a472b81b19ae15737c5e0cfbfd270e26
breenasaurus <62038408+breenasaurus@users.noreply.github.com>
2020-04-24 04:50:41 -0500

    Update main code
    Added if loop and while loop

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-execfile('HelloWorld.py')
+# This is the main file for th competition
+import testgui.py
+import Competition_Data_Handler.py 
+import Motor_Controls.py
 
 execfile('testgui.py')
+
+# Inputs TeamID and locates the line on Spreadsheet
+# I dont know how to do this part
+
+# The testing portion of this will originally pull down once and log the first value 
+# after that 
+def begintest():
+  command = Go_down
+  counter = 0
+  
+  
+  new_weight = # need input form stype code here
+  while counter >3
+   if new_weight >= weight
+      weight = new_weight
+      command = Go_Down
+   elif new_weight < weight
+      counter = counter + 1
+  
